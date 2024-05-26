8919a5cecc2c186c981284194fd18fd10c68865c
Umair Ahmed <umairjdu@gmail.com>
2024-05-26 16:56:00 +0500

    update

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+const a = "Module Number"
+const b = "1"
+const c = "Netro, Do you want to do that"
+
+export default a; 
---
+++ module2.mjs
@@ -0,0 +1,4 @@
+import nz from "./module1.mjs";
+console.log(nz);
+
+console.log("module is running... " + nz);
