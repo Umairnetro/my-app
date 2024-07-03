f73a44602ca9aa967e9b9a76178a01ef3aaa5a82
Umair Ahmed <umairjdu@gmail.com>
2024-07-03 10:46:25 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import "./App.css";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import React, { useState } from "react";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msge: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#222528";
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
   };
   return (
     <>
       {/* <Navbar title="React" aboutText="About on react" /> */}
       {/* <Navbar/> */}
-      <Navbar title="React" aboutText="About" mode={mode} toggleMode={toggleMode}/>
-      <Textform heading="Enter the text to analyze below" mode={mode}/>
+      <Navbar
+        title="React"
+        aboutText="About"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <Textform
+        showAlert={showAlert}
+        heading="Enter the text to analyze below"
+        mode={mode}
+      />
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -5,29 +5,34 @@ export default function Textform(props) {
     console.log("Uppercase button clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to uppercase!", "success");
   };
 
   const handleLowerClick = () => {
     console.log("Lowercase button clicked" + text);
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to lowercase!", "success")
   };
 
   const handleClearClick = () => {
     let newText = "";
     setText(newText);
+    props.showAlert("Text has been cleared", "success")
   };
 
   const handleCopyClick = () => {
     let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to clipboard!", "success")
   };
 
   // extra spaces
   const handleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
+    props.showAlert("Extra spaces removed!", "success")
   };
 
   const handleOnChange = (event) => {
